a30b9c3d3b209af8862f132d3492c0271543c514
Joshua Kulp <Joshua.Kulp@bentley.com>
2017-02-21 16:19:47 -0500

    Just a test

--- bot.js
+++ bot.js
@@ -1,9 +1,16 @@
 
 var retweet = function() {
+  console.log("1");
+  console.log("2");
+  console.log("3");
+  console.log("4");
+  console.log("5");
+  console.log("6");
+  console.log("7");
   console.log("Is this thing on?");
 }
 retweet();
-setInterval(retweet, 3000000);
+setInterval(retweet, 60000);
 
 
 
